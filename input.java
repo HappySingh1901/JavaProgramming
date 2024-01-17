b72c4fcef88cf22a58224d042453d400bdd18849
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-17 17:05:24 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        String userName=sc.nextLine();
+        System.out.print(userName);
+    }
+}
