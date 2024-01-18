3e05e2c5e0023b944808b0a66c05db5847325597
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-18 13:27:16 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,30 @@
+public class strings {
+    public static void main(String[] args){
+        String car="Volvo";
+        System.out.println(car); 
+
+        String txt="qwertyuiopasdfghjklzxcvbnm";
+        System.out.println(txt.length());
+
+        String name="Happy signh";
+        System.out.println(name.toUpperCase());
+
+        String name1="Happy signh";
+        System.out.println(name1.toLowerCase());
+
+        String common= "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+
+        System.out.println(common.indexOf("J"));
+        System.out.println(common.charAt(5));
+
+        String str1="Happy";
+        String str2=" Singh";
+
+        System.out.println(str1.concat(str2));              //We can simply add though
+        System.out.println(str1+str2);
+
+        int num1=100;
+        String str3=" Hundred";                             //If we add a number to string the resultant will be a string.
+        System.out.println(num1+str3);
+    }
+}
