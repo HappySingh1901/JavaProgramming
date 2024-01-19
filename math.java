ffa4649026d15cc8844f67f48b59cb14b6e0731c
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-19 11:46:46 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,18 @@
+public class math {
+    public static void main(String[] args){
+        
+        System.out.println(Math.max(4749,7982));               //It takes only two arguments.
+        
+        System.out.println(Math.min(47832,47839));
+
+        System.out.println( (int) Math.sqrt(4));                //(int) for intrger value otherwise the value would be in float.
+
+        System.out.println(Math.abs(-1.4223));
+
+        System.out.println((int)Math.pow(6,3));
+        
+        System.out.println(Math.ceil(-1.4223));
+
+        System.out.println(Math.floor(-1.4223));
+    }
+}
