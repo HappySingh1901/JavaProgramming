62decbe12ebd6c1179d5280aec379daf2b10c98a
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-22 23:42:24 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+    public static void main(String[] args) {
+        int num1=5;
+        num1+=5;
+        System.out.println(num1);                   //10
+        int num2=10;
+        System.out.println(num1%num2);              //0   //Modulus Operator 
+
+        num1++;
+        System.out.println(num1);                   //11
+        //num2++;
+        int y=num1++;
+        System.out.println(y);                      //11
+        y=++num1;
+        System.out.println(y);                      //13
+        System.out.println(num1);                   //13
+
+    }
+}
