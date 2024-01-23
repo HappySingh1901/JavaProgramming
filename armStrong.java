2c899f55b12666399c570368d5610c24abb37219
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-23 23:41:02 +0530

    Add files via upload

--- armStrong.java
+++ armStrong.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 public class armStrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num3 = sc.nextInt();
-        int temp = num3;
+        System.out.println("Enter a number");
+        String num3 = sc.nextLine();
+        int length= num3.length();
+        int num=Integer.parseInt(num3);
+        int temp = num;
         int m=0;
         while(temp>0){
             int b=temp%10;
-            m=m+ (int) Math.pow(b,3);
+            m=m+ (int) Math.pow(b,length);
             temp=temp/10;
         }
-        if (m==num3){
+        if (m==num){
             System.out.println("Armstrong Number");
         }else{
             System.out.println("Not Armstrong");
