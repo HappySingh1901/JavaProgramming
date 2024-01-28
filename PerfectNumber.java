a20ba3adac60dc4e751eefefff6043571051453d
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-01-28 11:38:17 +0530

    Add files via upload

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -2,6 +2,7 @@ import java.util.Scanner;;
 public class PerfectNumber {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number to check if the number if perfect number or not");
         int num = sc.nextInt();
         int m = 0;
         for(int i=1 ; i < num ; i++){
