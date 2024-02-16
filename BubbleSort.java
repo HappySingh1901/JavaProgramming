07b20b977c63bcc3738a8e478b9f425fbcd3f912
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-02-17 03:00:07 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[]={1,25,8,3,6,5,0};
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] +  " ");
+        }
+
+    }
+}
