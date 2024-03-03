5cb027c112238228ad5345fee7924c13146060cb
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-03-04 00:46:59 +0530

    Add files via upload

---
+++ StringToArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class StringToArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char c[]=s.toCharArray();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            System.out.println(c[i]);
+        }
+        sc.close();
+    }
+}
