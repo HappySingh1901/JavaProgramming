f08600591c474824e29f76d363efe372b4c78334
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-03-09 17:31:29 +0530

    Add files via upload

---
+++ CharInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class CharInput {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        System.out.println("The character inputted was " + c);
+        sc.close();
+    }
+    
+}
