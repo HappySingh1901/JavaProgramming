bb0184ff94b6cac43fd7221b820d277f2a7db0ca
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-03-10 14:28:43 +0530

    Add files via upload

--- CharInput.java
+++ CharInput.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 public class CharInput {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        char c=sc.next().charAt(0);
-        System.out.println("The character inputted was " + c);
+
+        //1st Method
+        //char c=sc.next().charAt(0);
+        //System.out.println("The character inputted was " + c);
+
+
+
+        //Another Method 
+        System.out.println("Enter a character: ");
+        String input = sc.nextLine();
+
+        // Check if input is a single character
+        if(input.length() != 1) {
+            System.out.println("Please enter only one character.");
+            return;
+        }
+
+        char character = input.charAt(0);
+        System.out.println("Character entered: " + character);
+
+
+
         sc.close();
     }
     
