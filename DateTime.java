87595a16b4f070ed06e1856380a8428e7c2ea3b2
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-03-27 00:32:07 +0530

    Add files via upload

--- DateTime.java
+++ DateTime.java
@@ -1,11 +1,14 @@
 import java.time.LocalDate; // import the LocalDate class
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class DateTime {
     public static void main(String[] args) {
     LocalDate myObj = LocalDate.now(); // Create a date object
     System.out.println(myObj); // Display the current date
-    LocalTime myObj1 = LocalTime.now();
+    LocalTime myObj1 = LocalTime.now(); 
     System.out.println(myObj1);
+    LocalDateTime myObj2=LocalDateTime.now();
+    System.out.println(myObj2); 
     }
 }
