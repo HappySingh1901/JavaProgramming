691968d639feca5a17b264bd29e97370664f1cd4
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-04-09 16:27:20 +0530

    Add files via upload

---
+++ TryCatch2.java
@@ -0,0 +1,10 @@
+public class TryCatch2 {
+    public static void main(String[ ] args) {
+      try {
+        int[] myNumbers = {1, 2, 3};
+        System.out.println(myNumbers[10]);
+      } catch (Exception e) {
+        System.out.println("Something went wrong.");
+      }
+    }
+  }
