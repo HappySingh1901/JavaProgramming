4a3c3c8686e7758a2a250fc3cc7fab128264cb46
Happy Singh <118984911+HappySingh1901@users.noreply.github.com>
2024-05-06 02:46:36 +0530

    Add files via upload

---
+++ Collections5Arr.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Collections5Arr {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // arr.add(5);
+        // arr.add(6);
+        // arr.add(4);
+        // arr.add(8);
+        // arr.add(7);
+        // arr.add(2);
+        // List<Integer> a=arr.subList(2,4);           //SubList
+        // for(int i=a.size()-1;i>=0;i--){
+        //     System.out.println(a.get(i));
+        // }
+
+        // System.out.println(a);
+        // Scanner sc=new Scanner(System.in);
+        // String s=sc.nextLine();
+        // ArrayList<Integer> arr1=new ArrayList<>();
+        // String []arr2=s.split(" ");
+        // // System.out.println(arr2.length);
+        // for(int i=0;i<arr2.length;i++){
+        //     arr1.add(Integer.parseInt(arr2[i]));
+        // }
+        // System.out.println(arr1);
+        // sc.close();
+
+
+
+
+    }
+}
